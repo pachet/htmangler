51b250d272e867d686958f2c4c92a1e1377272bd
pachet <pachet@burninggarden.com>
2017-08-21 20:32:28 +0000

    Add flag on HTMLNode for indicating whether a node represents an HTML end tag

--- lib/html-node.rb
+++ lib/html-node.rb
@@ -2,4 +2,14 @@ require_relative "markov-node"
 
 class HTMLNode < MarkovNode
 
+  attr_accessor :is_end_tag
+
+  alias is_end_tag? is_end_tag
+
+  def initialize(tokens)
+    super(tokens)
+
+    @is_end_tag = false
+  end
+
 end
